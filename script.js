28d9c216ccb65cde0e53d4dd45efd28172c8bff6
Dean Warwick <elborro@gmail.com>
2022-06-01 13:12:42 +0200

    added DATA2 to script.js

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 // DATA 1 //
-const markHeight = 1.69
-const markWeight = 78
+const markHeight = 1.69;
+const markWeight = 78;
 
-const johnHeight = 1.95
-const johnWeight = 92
+const johnHeight = 1.95;
+const johnWeight = 92;
 
-const johnBmi = ( johnWeight / (johnHeight * johnHeight) )
+const johnBmi = ( johnWeight / (johnHeight * johnHeight) );
 
-console.log(johnBmi)
+console.log(johnBmi);
 
-const markBmi = ( markWeight / (markHeight * markHeight))
+const markBmi = ( markWeight / (markHeight * markHeight));
 
-console.log(markBmi)
+console.log(markBmi);
 
 const markHigherBmi = markBmi > johnBmi;
 
-console.log(markHigherBmi)
+console.log(markHigherBmi);
 
 
 // DATA 2 //
 
+const heightMark = 1.88;
+const massMark = 95;
+
+const heightJohn = 1.76;
+const massJohn = 85;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / heightJohn ** 2;
+
+console.log(bmiMark, bmiJohn);
+
+const higherBmiMark = bmiMark > bmiJohn;
+
+console.log( higherBmiMark);
