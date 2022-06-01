b81e9195769f7198895cf8b7355f6f4f235add6c
Dean Warwick <elborro@gmail.com>
2022-06-01 13:03:38 +0200

    Challenge #1

---
+++ script.js
@@ -0,0 +1,22 @@
+// DATA 1 //
+const markHeight = 1.69
+const markWeight = 78
+
+const johnHeight = 1.95
+const johnWeight = 92
+
+const johnBmi = ( johnWeight / (johnHeight * johnHeight) )
+
+console.log(johnBmi)
+
+const markBmi = ( markWeight / (markHeight * markHeight))
+
+console.log(markBmi)
+
+const markHigherBmi = markBmi > johnBmi;
+
+console.log(markHigherBmi)
+
+
+// DATA 2 //
+
